475c964574ddf00c7ba73cf6ed04052503a47ee5
JonathanBriers <jonathan.briers@btinternet.com>
2013-12-09 11:15:08 +0000

    First commit

---
+++ labtest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labtest::Application.config.secret_key_base = '3cd739da8d1eea914a6f471f266c7cc74b7a3f7c214821f82e8bbc8462e9d8c19b4bee85df9ed84152b675e6b3eca04d22e2c18e2658c104e456cd7a6ec072ce'
